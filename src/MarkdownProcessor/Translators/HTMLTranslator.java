47f79aa4cb9cbd65ce74e5f8db13f5bac0c5a756
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-16 13:56:16 +1300

    Created HTML translator impl

--- src/MarkdownProcessor/Translators/HTMLTranslator.java
+++ src/MarkdownProcessor/Translators/HTMLTranslator.java
@@ -1,4 +1,40 @@
 package MarkdownProcessor.Translators;
 
-public class HTMLTranslator {
+import MarkdownProcessor.Nodes.*;
+
+public class HTMLTranslator implements Translator {
+    @Override
+    public String translateHeader(HeaderNode node) {
+        return null;
+    }
+
+    @Override
+    public String translateLine(LineNode node) {
+        return null;
+    }
+
+    @Override
+    public String translateEmphasis(EmphasisNode node) {
+        return null;
+    }
+
+    @Override
+    public String translateBold(BoldNode node) {
+        return null;
+    }
+
+    @Override
+    public String translateParagraph(ParagraphNode node) {
+        return null;
+    }
+
+    @Override
+    public String translateMarkdownFile(MarkdownFileNode node) {
+        return null;
+    }
+
+    @Override
+    public String translateString(StringNode node) {
+        return null;
+    }
 }
