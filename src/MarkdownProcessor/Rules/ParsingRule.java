8ac78878619c3d4535f74da3c6ca9da989e051c7
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-17 10:18:03 +1300

    Created Rules package and ParsingRule interface to specify the conditions and actions regarding the different identifiers in markdown e.g. asterisks

---
+++ src/MarkdownProcessor/Rules/ParsingRule.java
@@ -0,0 +1,24 @@
+package MarkdownProcessor.Rules;
+
+import MarkdownProcessor.Nodes.TextNode;
+
+/**
+ * Specifies the functionalities of parsing rules such as the conformity to certain conditions,
+ * and the actions applied to each rule.
+ */
+public interface ParsingRule {
+
+    /**
+     * Returns true if the given string conforms to the rule.
+     * @param s
+     * @return true if it fits the rule's conditions, false otherwise.
+     */
+    boolean condition(String s);
+
+    /**
+     * Returns the TextNode that is created when the rule is applied.
+     * @param s
+     * @return TextNode representing the rule applied to the given string.
+     */
+    TextNode action(String s);
+}
