d45932578f92811e1833be2dadcd490c3ea32e50
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-16 14:40:39 +1300

    Added HTML Translator method logic

--- src/MarkdownProcessor/Translators/HTMLTranslator.java
+++ src/MarkdownProcessor/Translators/HTMLTranslator.java
@@ -2,39 +2,45 @@ package MarkdownProcessor.Translators;
 
 import MarkdownProcessor.Nodes.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class HTMLTranslator implements Translator {
     @Override
     public String translateHeader(HeaderNode node) {
-        return null;
+        return "<h" + node.getDepth() + ">" +
+                Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) +
+                "</h" + node.getDepth() + ">";
     }
 
     @Override
     public String translateLine(LineNode node) {
-        return null;
+        return Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining());
     }
 
     @Override
     public String translateEmphasis(EmphasisNode node) {
-        return null;
+        return "<em>" + Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) + "</em>";
     }
+
 
     @Override
     public String translateBold(BoldNode node) {
-        return null;
+        return "<strong>" + Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) + "</strong>";
     }
 
     @Override
     public String translateParagraph(ParagraphNode node) {
-        return null;
+        return "<p>" + Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) + "</p>";
     }
 
     @Override
     public String translateMarkdownFile(MarkdownFileNode node) {
-        return null;
+        return Stream.of(node).map(n -> n.accept(this) + "\n").collect(Collectors.joining());
     }
 
     @Override
     public String translateString(StringNode node) {
-        return null;
+        return node.toString();
     }
 }
