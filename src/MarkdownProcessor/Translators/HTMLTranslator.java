927d86cecb57c2ad666da45f9faa0346b9b7e5bf
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-17 10:01:56 +1300

    Refactored HTMLTranslator to accomodate changes to Nodes i.e. utilize getChildren

--- src/MarkdownProcessor/Translators/HTMLTranslator.java
+++ src/MarkdownProcessor/Translators/HTMLTranslator.java
@@ -3,40 +3,57 @@ package MarkdownProcessor.Translators;
 import MarkdownProcessor.Nodes.*;
 
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class HTMLTranslator implements Translator {
     @Override
     public String translateHeader(HeaderNode node) {
         return "<h" + node.getDepth() + ">" +
-                Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) +
+                node.getChildren().stream()
+                        .map(n -> n.accept(this))
+                        .collect(Collectors.joining()) +
                 "</h" + node.getDepth() + ">";
     }
 
     @Override
     public String translateLine(LineNode node) {
-        return Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining());
+        return node.getChildren().stream()
+                .map(n -> n.accept(this))
+                .collect(Collectors.joining());
     }
 
     @Override
     public String translateEmphasis(EmphasisNode node) {
-        return "<em>" + Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) + "</em>";
+        return "<em>" +
+                node.getChildren().stream()
+                        .map(n -> n.accept(this))
+                        .collect(Collectors.joining()) +
+                "</em>";
     }
 
 
     @Override
     public String translateBold(BoldNode node) {
-        return "<strong>" + Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) + "</strong>";
+        return "<strong>" +
+                node.getChildren().stream()
+                        .map(n -> n.accept(this))
+                        .collect(Collectors.joining()) +
+                "</strong>";
     }
 
     @Override
     public String translateParagraph(ParagraphNode node) {
-        return "<p>" + Stream.of(node).map(n -> n.accept(this)).collect(Collectors.joining()) + "</p>";
+        return "<p>" +
+                node.getChildren().stream()
+                        .map(n -> n.accept(this))
+                        .collect(Collectors.joining()) +
+                "</p>";
     }
 
     @Override
     public String translateMarkdownFile(MarkdownFileNode node) {
-        return Stream.of(node).map(n -> n.accept(this) + "\n").collect(Collectors.joining());
+        return node.getChildren().stream()
+                .map(n -> n.accept(this) + "\n") //TODO: confirm \n is needed here
+                .collect(Collectors.joining());
     }
 
     @Override
