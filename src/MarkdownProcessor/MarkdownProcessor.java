453e0acd9d233168175441ae6ac9990e911868af
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-16 14:41:42 +1300

    Fixed MarkdownProcessor to have no scanner field, but local variable in parse method

--- src/MarkdownProcessor/MarkdownProcessor.java
+++ src/MarkdownProcessor/MarkdownProcessor.java
@@ -8,24 +8,15 @@ import java.util.Scanner;
  * Markdown file processor. Generates syntax tree of given input.
  */
 public class MarkdownProcessor {
-    private Scanner scanner;
 
-    /**
-     * Processor takes scanner that reads input string/file.
-     *
-     * @param scanner
-     */
-    public MarkdownProcessor(Scanner scanner) {
-        //TODO: Determine how to specify output file. Ask for filename or final field?
-        this.scanner = scanner;
-    }
+    public MarkdownProcessor() {}
 
     /**
      * Parses markdown lines into LineNodes.
      *
      * @return List<ParserNode> representing the text in the scanner.
      */
-    public MarkdownFileNode parseMarkdown() {
+    public MarkdownFileNode parseMarkdown(Scanner scanner) {
         return new MarkdownFileNode(scanner);
     }
 }
