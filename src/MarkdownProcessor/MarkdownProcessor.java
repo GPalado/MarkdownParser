ada5e8bbb73fe11e18870ceaff5bf645d55a09aa
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-17 15:22:51 +1300

    Rewrote MarkdownProcessor parse method to apply Rule to parse

--- src/MarkdownProcessor/MarkdownProcessor.java
+++ src/MarkdownProcessor/MarkdownProcessor.java
@@ -1,6 +1,7 @@
 package MarkdownProcessor;
 
-import MarkdownProcessor.Nodes.MarkdownFileNode;
+import MarkdownProcessor.Nodes.TextNode;
+import MarkdownProcessor.Rules.ParserHelper;
 
 import java.util.Scanner;
 
@@ -14,9 +15,9 @@ public class MarkdownProcessor {
     /**
      * Parses markdown lines into LineNodes.
      *
-     * @return List<ParserNode> representing the text in the scanner.
+     * @return TextNode representing the text in the scanner.
      */
-    public MarkdownFileNode parseMarkdown(Scanner scanner) {
-        return new MarkdownFileNode(scanner);
+    public TextNode parseMarkdown(Scanner scanner) {
+        return ParserHelper.applyStructureRules(scanner);
     }
 }
