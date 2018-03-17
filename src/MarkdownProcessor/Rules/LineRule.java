271a9c834a601c994ca98a8d6b7c98e1933b47c5
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-17 17:28:27 +1300

    Completed implementations of remaining structural rules

--- src/MarkdownProcessor/Rules/LineRule.java
+++ src/MarkdownProcessor/Rules/LineRule.java
@@ -1,4 +1,25 @@
 package MarkdownProcessor.Rules;
 
-public class LineRule {
+import MarkdownProcessor.Nodes.LineNode;
+import MarkdownProcessor.Nodes.TextNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LineRule implements StructureRule {
+
+    public LineRule(){}
+
+    @Override
+    public TextNode applyStructure(Scanner s) {
+        System.out.println("Line applyStructure");
+        List<TextNode> children = new ArrayList<>();
+        while(s.hasNext()){
+            System.out.println("Line s.hasNext");
+            children.add(ParserHelper.applyEffectRules(s));
+        }
+        System.out.println("Line returned");
+        return new LineNode(children);
+    }
 }
--- src/MarkdownProcessor/Rules/MarkdownFileRule.java
+++ src/MarkdownProcessor/Rules/MarkdownFileRule.java
@@ -1,4 +1,23 @@
 package MarkdownProcessor.Rules;
 
-public class MarkdownFileRule {
+import MarkdownProcessor.Nodes.MarkdownFileNode;
+import MarkdownProcessor.Nodes.TextNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MarkdownFileRule implements StructureRule {
+
+    public MarkdownFileRule(){}
+
+    @Override
+    public TextNode applyStructure(Scanner s) {
+        System.out.println("Markdown applyStructure");
+        List<TextNode> paragraphs = new ArrayList<>();
+        while(s.hasNextLine()){
+            paragraphs.add(new ParagraphRule().applyStructure(s));
+        }
+        return new MarkdownFileNode(paragraphs);
+    }
 }
