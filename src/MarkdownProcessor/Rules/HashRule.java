b328990f1c1d486adacd47fa9576a96e4a281730
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-17 19:39:58 +1300

    Completed implementation of the HashRule and tested

--- src/MarkdownProcessor/Rules/HashRule.java
+++ src/MarkdownProcessor/Rules/HashRule.java
@@ -3,21 +3,55 @@ package MarkdownProcessor.Rules;
 import MarkdownProcessor.Nodes.HeaderNode;
 import MarkdownProcessor.Nodes.TextNode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * Implementation of the Hash Rule, in which one or more '#' followed by a space denotes a header.
+ * Implementation of the Hash Rule, in which one or more '#' followed by one or more
+ * spaces denotes a header.
  */
-public class HashRule implements ParsingRule {
+public class HashRule implements EffectRule {
 
-    public HashRule(){}
-
-    @Override
-    public boolean meetsCondition(String s) {
-        //TODO: fix regex to accept multiple hashes
-        return s.startsWith("[#]+ ");
+    public HashRule() {
     }
 
     @Override
-    public TextNode action(String s) {
-        return new HeaderNode(s);
+    public boolean meetsCondition(Scanner s) {
+        s.useDelimiter("\n");
+        return s.hasNext("#+[\\s]+[a-zA-Z0-9//\\\\`~!@#$%^&*()-=_+\\[\\]{}|'\",?><,.:;]+");
+    }
+
+    @Override
+    public TextNode applyAction(Scanner s) {
+        if (meetsCondition(s)) {
+            int depth = filterHashesAndSpaces(s);
+            return new HeaderNode(depth > 5 ? 5 : depth, parseChildren(s));
+        }
+        throw new IllegalArgumentException("HashRule cannot be applied to the given input.");
+    }
+
+    private List<TextNode> parseChildren(Scanner s){
+        List<TextNode> children = new ArrayList<>();
+        String line = s.nextLine();
+        Scanner lineScanner = new Scanner(line);
+        while(lineScanner.hasNext()){
+            TextNode child = ParserHelper.applyEffectRules(lineScanner);
+            children.add(child);
+        }
+        return children;
+    }
+
+    /**
+     * Filters the hashes and spaces from the start of the given scanner.
+     * @param scanner
+     * @return number of hashes at the start of the scanner input.
+     */
+    private int filterHashesAndSpaces(Scanner scanner) {
+        scanner.reset();
+        String hashes = scanner.next();
+        System.out.println(hashes+"|");
+        scanner.skip("[\\s]*");
+        return hashes.length();
     }
 }
