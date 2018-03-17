827f9993597eecfeeb9fe8c9691db26501872532
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-17 16:31:35 +1300

    Abstracted out both effect rules e.g. header, and structure rules e.g. paragraph, for parsing

--- src/MarkdownProcessor/Rules/EffectRule.java
+++ src/MarkdownProcessor/Rules/EffectRule.java
@@ -2,27 +2,25 @@ package MarkdownProcessor.Rules;
 
 import MarkdownProcessor.Nodes.TextNode;
 
-import java.util.Optional;
+import java.util.Scanner;
 
 /**
- * Specifies the functionalities of parsing rules such as the conformity to certain conditions,
+ * Specifies the functionalities of effect rules such as the conformity to certain conditions,
  * and the actions applied to each rule.
  */
-public interface ParsingRule {
-    //TODO: Concerns about sequential requirements for calls to these methods.s
+public interface EffectRule {
 
     /**
      * Returns true if the given string conforms to the rule.
      * @param s
      * @return true if it fits the rule's conditions, false otherwise.
      */
-    boolean meetsCondition(String s);
+    boolean meetsCondition(Scanner s);
 
     /**
      * Returns the TextNode that is created when the rule is applied.
      * @param s
-     * @return Optional of the TextNode representing the rule applied to the given string if the string meets the condition,
-     * Optional.empty() otherwise.
+     * @return TextNode representing the rule applied to the given string.
      */
-    Optional<TextNode> applyAction(String s);
+    TextNode applyAction(Scanner s);
 }
--- src/MarkdownProcessor/Rules/StructureRule.java
+++ src/MarkdownProcessor/Rules/StructureRule.java
@@ -1,4 +1,18 @@
 package MarkdownProcessor.Rules;
 
+import MarkdownProcessor.Nodes.TextNode;
+
+import java.util.Scanner;
+
+/**
+ * Describes a rule specific to the structure of the input.
+ */
 public interface StructureRule {
+
+    /**
+     * Applies the structure rule to the given scanner input.
+     * @param s
+     * @return Optional of a TextNode containing the structure if possible, Optional.empty() otherwise.
+     */
+    TextNode applyStructure(Scanner s);
 }
