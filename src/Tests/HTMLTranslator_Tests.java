e813738c363edcb35d33cdbfe7bbbae6c55b2597
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-21 13:44:14 +1300

    Added tests for just translating nodes

--- src/Tests/HTMLTranslator_Tests.java
+++ src/Tests/HTMLTranslator_Tests.java
@@ -1,11 +1,16 @@
 package Tests;
 
 import MarkdownProcessor.Nodes.*;
+import MarkdownProcessor.Translators.HTMLTranslator;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class HTMLTranslator_Tests {
+    private static final HTMLTranslator translator = new HTMLTranslator();
 
     private static BoldNode getBoldNode(List<TextNode> children) {
         return new BoldNode(children);
@@ -35,6 +40,50 @@ public class HTMLTranslator_Tests {
         return new StringNode(string);
     }
 
-//    @Test
-//    public void translate_
+    @Test
+    public void translate_header_test(){
+        HeaderNode header = getHeaderNode(Arrays.asList(getStringNode("I am child")), 1);
+        assertEquals("<h1>I am child</h1>", header.accept(translator));
+    }
+
+    @Test
+    public void translate_paragraph_test(){
+        ParagraphNode paragraph = getParagraphNode(Arrays.asList(getStringNode("I am para body")));
+        assertEquals("<p>I am para body</p>", paragraph.accept(translator));
+    }
+
+    @Test
+    public void translate_bold_test(){
+        BoldNode bold = getBoldNode(Arrays.asList(getStringNode("I am bold")));
+        assertEquals("<strong>I am bold</strong>", bold.accept(translator));
+    }
+
+    @Test
+    public void translate_italics_test(){
+        EmphasisNode italics = getEmphasisNode(Arrays.asList(getStringNode("I am italics")));
+        assertEquals("<em>I am italics</em>", italics.accept(translator));
+    }
+
+    @Test
+    public void translate_lineWithBoldAndString_test(){
+        LineNode line = getLineNode(Arrays.asList(getStringNode("I am normal"), getBoldNode(Arrays.asList(getStringNode("I am bold")))));
+        assertEquals("I am normal<strong>I am bold</strong>", line.accept(translator));
+    }
+
+    @Test
+    public void translate_MarkdownWithHeaderAndPara_test(){
+        MarkdownFileNode markdown = getMarkdownFileNode(
+                Arrays.asList(
+                        getHeaderNode(Arrays.asList(getStringNode("I am header")), 1),
+                        getParagraphNode(Arrays.asList(getStringNode("I am paragraph")))
+                )
+        );
+        assertEquals("<h1>I am header</h1>\n<p>I am paragraph</p>\n", markdown.accept(translator));
+    }
+
+    @Test
+    public void translate_string_test(){
+        StringNode string = getStringNode("I am string");
+        assertEquals("I am string", string.accept(translator));
+    }
 }
