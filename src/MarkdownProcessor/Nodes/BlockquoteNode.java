c648ec367d79bdfd2529c3cefe603f6f6e475372
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2018-03-24 19:42:36 +1300

    Created BlockquoteNode

--- src/MarkdownProcessor/Nodes/BlockquoteNode.java
+++ src/MarkdownProcessor/Nodes/BlockquoteNode.java
@@ -1,4 +1,36 @@
 package MarkdownProcessor.Nodes;
 
-public class BlockquoteNode {
+import MarkdownProcessor.Translators.Translator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BlockquoteNode implements CollectorNode {
+    private List<TextNode> children;
+
+    public BlockquoteNode(List<TextNode> children){
+        this.children = new ArrayList<>();
+        this.children.add(new ParagraphNode(children));
+    }
+
+    @Override
+    public List<TextNode> getChildren() {
+        return Collections.unmodifiableList(children);
+    }
+
+    @Override
+    public String accept(Translator t) {
+        return t.translateBlockquote(this);
+    }
+
+    @Override
+    public String toString(){
+        return "Blockquote[" +
+                children.stream()
+                .map(child -> child.toString())
+                .collect(Collectors.joining()) +
+                "]";
+    }
 }
